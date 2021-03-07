faf8089a6391c9a77a50e20ad8b8f8bd2fd1a3eb
fbnhyn <fabian.heyne@outlook.com>
2021-03-07 20:12:39 +0100

    test: add more static tests

--- test/test_lemon_spider.py
+++ test/test_lemon_spider.py
@@ -31,5 +31,38 @@ class TestLemonSpider(unittest.TestCase):
         lemon =  next(result)
         pprint(lemon)
 
+    def test_parse_hyundai_i10(self):
+        self.response = TextResponse(
+            url='www.lemonunittest.de',
+            body=open(f'tmp\\hyundai.i10.htm', encoding='utf-8').read(),
+            encoding = 'utf-8'
+        )
+
+        result = self.spider.parse_lemon(self.response)
+        lemon =  next(result)
+        pprint(lemon)
+
+    def test_parse_renault_twingo(self):
+        self.response = TextResponse(
+            url='www.lemonunittest.de',
+            body=open(f'tmp\\renault.twingo.htm', encoding='utf-8').read(),
+            encoding = 'utf-8'
+        )
+
+        result = self.spider.parse_lemon(self.response)
+        lemon =  next(result)
+        pprint(lemon)
+
+    def test_parse_renault_zoe(self):
+        self.response = TextResponse(
+            url='www.lemonunittest.de',
+            body=open(f'tmp\\renault.zoe.htm', encoding='utf-8').read(),
+            encoding = 'utf-8'
+        )
+
+        result = self.spider.parse_lemon(self.response)
+        lemon =  next(result)
+        pprint(lemon)
+
 if __name__ == '__main__':
     unittest.main()
