d2795e5818e013a58882c498d60bb2968ad2f914
fbnhyn <fabian.heyne@outlook.com>
2021-04-02 16:02:54 +0200

    chore: make query refinement faster

--- lmpd/lemon/lemon/models.py
+++ lmpd/lemon/lemon/models.py
@@ -25,41 +25,34 @@ class Query:
             self.query['priceto'] = [str(self.first_price_cap)]
             return self.__compose_new_url()
         if self.hits > self.hit_cap:
-            return self.__refine_lower_query()
-        return self.__refine_upper_query()
+            return self.__refine_lower()
+        return self.__refine_upper()
 
     def has_upper(self):
-        if self.price_to is None and self.hits <= 200:
-            return False
-        if self.price_to == self.max_price:
-            return False
+        if self.price_to is None and self.hits <= self.hit_cap: return False
+        if self.price_to == self.max_price: return False
         return True
 
-    def __refine_lower_query(self):
+    def __refine_lower(self):
         if self.price_to is None:
-            if self.price_from is not None:
-                self.query['priceto'] = [str(self.price_from * 5)]
-            else:
-                self.query['priceto'] = ['100000']
+            if self.price_from is not None: self.query['priceto'] = [str(self.price_from * 2)]
+            else: self.query['priceto'] = ['100000']
         else:
             price_range = self.price_to - self.price_from
             divider = self.__get_divider(price_range)
             self.query['priceto'] = [str(math.floor((self.price_from + self.price_to) / divider))]
         return self.__compose_new_url()
 
-    def __refine_upper_query(self):
-        if self.price_to is None:
-            self.query['pricefrom'] = [str(self.price_from * 5)]
+    def __refine_upper(self):
+        if self.price_to is None: self.query['pricefrom'] = [str(self.price_from * 2)]
         else:
             self.query['pricefrom'] = [str(self.price_to + 1)]
             self.query.pop('priceto')
         return self.__compose_new_url()
 
     def __get_divider(self, price_range: int):
-        if price_range > 500000:
-            return 8
-        if price_range > 100000:
-            return 4
+        if price_range > 500000:  return 8
+        if price_range > 100000: return 4
         return 2
 
     def __compose_new_url(self):
