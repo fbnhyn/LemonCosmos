b51101b2e0c732a01f5bb66ebc7c136294167c8d
fbnhyn <fabian.heyne@outlook.com>
2021-04-02 14:51:48 +0200

    chore: make query spider more readable

--- lmpd/lemon/lemon/spiders/query_spider.py
+++ lmpd/lemon/lemon/spiders/query_spider.py
@@ -1,5 +1,7 @@
+from scrapy.http.response.text import TextResponse
 from lmpd.lemon.lemon import models
 import scrapy
+import logging
 import math
 import sys
 
@@ -10,26 +12,32 @@ class QuerySpider(scrapy.Spider):
 
     def parse(self, response):
         try:
-            query_hits = int(response.css('.cl-filters-summary-counter::text').extract_first().replace('.', ""))
-            q = models.Query(response, query_hits)
-            if query_hits > 200:
-                print(f'refing query for {response.url}; {query_hits}')
-                yield scrapy.Request(q.refine_query(), callback=self.parse, dont_filter=True)
-            elif query_hits > 0 and query_hits <= 200:
-                self.result.hits += query_hits
-                if query_hits > 20:
-                    pages = math.ceil((query_hits / 20)) + 1
-                    for p in range(1, pages):
-                        print(f'adding {response.url}&page={p}')
-                        self.result.urls.append(response.url + f'&page={p}')
-                else:
-                    print(f'adding {response.url}')
-                    self.result.urls.append(response.url)
-                print(f'result_hits: {self.result.hits} ({len(self.result.urls)})')
-                if q.has_upper_query:
-                    yield scrapy.Request(q.refine_query(), callback=self.parse, dont_filter=True)
+            self.logger.info(response.url)
+            query = models.Query(response, self.parse_query_result(response))
+            print(f'{query.hits}\tby {response.url}')
+            if (query.hits == 0): return 
+            if (query.hits > query.hit_cap): yield scrapy.Request(query.refine(), callback=self.parse, dont_filter=True)
+            else:
+                self.result.hits += query.hits
+                self.add_query(response, query)
+                print(f'[{self.result.hits}]')
+                if query.has_upper(): yield scrapy.Request(query.refine(), callback=self.parse, dont_filter=True)
         except AttributeError:
             yield scrapy.Request(response.url, callback=self.parse, dont_filter=True)
         except:
             e = sys.exc_info()
             print(f'EXCEPTION {e[1]}')
+
+    def parse_query_result(self, response: TextResponse):
+        hasEmptyTitleClass = len(response.css('.cl-list-header-title-empty')) > 0
+        if (hasEmptyTitleClass): return 0
+        return int(response.css('.cl-filters-summary-counter>span::text').extract_first().replace('.', ""))
+
+    def add_query(self, response: TextResponse, query: models.Query):
+        if (query.hits > 20): self.add_query_pagination(response, query)
+        else: self.result.urls.append(response.url)
+
+    def add_query_pagination(self, response: TextResponse, query: models.Query):
+        pages = math.ceil((query.hits / 20)) + 1
+        for p in range(1, pages):
+            self.result.urls.append(response.url + f'&page={p}')
